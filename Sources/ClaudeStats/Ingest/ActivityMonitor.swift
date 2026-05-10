6ee23ed303122e7472d566a9cbbba044528cd32e
Jan Jaap <mail@janjaap.de>
2026-05-10 16:09:06 +0200

    ingest: add ActivityMonitor wrapping reader with periodic timer

---
+++ Sources/ClaudeStats/Ingest/ActivityMonitor.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+@MainActor
+final class ActivityMonitor {
+    private let reader: UsageReader
+    private let interval: TimeInterval
+    private let activeThreshold: TimeInterval
+    private var timer: Timer?
+
+    var onTick: (() -> Void)?
+
+    init(reader: UsageReader, interval: TimeInterval = 10, activeThreshold: TimeInterval = 30) {
+        self.reader = reader
+        self.interval = interval
+        self.activeThreshold = activeThreshold
+    }
+
+    func start() {
+        stop()
+        let t = Timer(timeInterval: interval, repeats: true) { [weak self] _ in
+            Task { @MainActor in self?.tickNow() }
+        }
+        RunLoop.main.add(t, forMode: .common)
+        timer = t
+        tickNow()
+    }
+
+    func stop() { timer?.invalidate(); timer = nil }
+
+    func tickNow() {
+        try? reader.scan()
+        onTick?()
+    }
+
+    var isActive: Bool { reader.isActive(within: activeThreshold) }
+}
